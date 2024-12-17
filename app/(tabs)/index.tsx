9cc5236f221cb8f0d363a33e8a597f1e3172c611
Mickael Lecoq <mickael@lecoq.me>
2024-12-17 17:07:55 +0100

    init example

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,56 +1,85 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {Image, StyleSheet, Platform, View, Text} from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {SvgUri} from 'react-native-svg';
+import {useEffect, useState} from 'react';
+import {BlendColor, Canvas, fitbox, Group, ImageSVG, Paint, rect, Skia, SkSVG} from '@shopify/react-native-skia';
 
 export default function HomeScreen() {
+  const [svg, setSVG] = useState<SkSVG | null>(null);
+  useEffect(() => {
+    let canceled = false;
+    const fetchSvg = async () => {
+      try {
+        const resp = await fetch(
+          'https://res.cloudinary.com/azzapp-dev/image/upload/q_auto:best/y7rwlt0937ff4dgulmv1ywdm',
+        );
+        if (canceled) {
+          return;
+        }
+        if (!resp.ok) {
+          throw new Error(
+            `Failed to fetch ${'https://res.cloudinary.com/azzapp-dev/image/upload/q_auto:best/y7rwlt0937ff4dgulmv1ywdm'}`,
+          );
+        }
+        const svgSrc = await resp.text();
+        if (canceled) {
+          return;
+        }
+        setSVG(Skia.SVG.MakeFromString(svgSrc));
+      } catch (e) {
+        if (canceled) {
+          return;
+        }
+        console.warn(
+          `Failed to load SVG: ${'https://res.cloudinary.com/azzapp-dev/image/upload/q_auto:best/y7rwlt0937ff4dgulmv1ywdm'}`,
+          e,
+        );
+
+        setSVG(null);
+      }
+    };
+    fetchSvg();
+
+    return () => {
+      canceled = true;
+    };
+  }, []);
+
+  const svgWidth = svg?.width() ?? 0;
+  const svgHeight = svg?.height() ?? 0;
+
+  const src = rect(0, 0, svgWidth, svgHeight);
+  const dst = rect(0, 0, 200, 200);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>react-native-svg</Text>
+      <View style={{width: 200, height: 200, borderWidth: 1, borderColor: 'black'}} pointerEvents="none">
+        <SvgUri
+          width="100%"
+          height="100%"
+          uri={'https://res.cloudinary.com/azzapp-dev/image/upload/q_auto:best/y7rwlt0937ff4dgulmv1ywdm'}
+          color={'#fff444'}
+          preserveAspectRatio="xMidYMid slice"
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+      <Text>react-native-skia</Text>
+      {svg && (
+        <Canvas style={{width: 200, height: 200}} opaque>
+          <Group
+            transform={fitbox('fill', src, dst)}
+            layer={
+              <Paint>
+                <BlendColor color={'#fff444'} mode="srcATop" />
+              </Paint>
+            }
+          >
+            <ImageSVG svg={svg} width={svgWidth} height={svgHeight} />
+          </Group>
+        </Canvas>
+      )}
+    </View>
   );
 }
 
